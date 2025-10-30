e67844896c29793e69cc86b4273fc1a6f55c97e3
Alexandr Kolosov <Ryver.rainege@gmail.com>
2025-10-30 11:37:39 +0300

    init

---
+++ src/main/java/com/kolosov/testprojectminnumberfromxlsx/TestProjectMInNumberFromXlsxApplication.java
@@ -0,0 +1,13 @@
+package com.kolosov.testprojectminnumberfromxlsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectMInNumberFromXlsxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectMInNumberFromXlsxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kolosov/testprojectminnumberfromxlsx/TestProjectMInNumberFromXlsxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolosov.testprojectminnumberfromxlsx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectMInNumberFromXlsxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
